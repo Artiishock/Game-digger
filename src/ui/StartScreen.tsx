cd4c4b93fe9d1a725f5aa4a079d058aa24706c1d
kagami <evb1404@gmail.com>
2026-05-15 17:35:24 +0300

    замена стрелок в онке с правилами

--- src/ui/StartScreen.tsx
+++ src/ui/StartScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { t } from "../i18n/t";
 import { resolvePublicUrl } from "../utils/publicUrl";
 
@@ -13,6 +13,58 @@ const slides = [
   { image: resolvePublicUrl("rules/treats.png"),      alt: "Treats",      titleKey: "avoid threats" },
   { image: resolvePublicUrl("rules/places.png"),      alt: "Places",      titleKey: "get to safe place" },
 ];
+
+interface InlineSvgImageProps {
+  idPrefix: string;
+  src: string;
+}
+
+const prefixSvgIds = (markup: string, prefix: string) =>
+  markup
+    .replace(/\sid="([^"]+)"/g, ` id="${prefix}-$1"`)
+    .replace(/url\(#([^)]+)\)/g, `url(#${prefix}-$1)`)
+    .replace(/href="#([^"]+)"/g, `href="#${prefix}-$1"`);
+
+const InlineSvgImage: React.FC<InlineSvgImageProps> = ({ idPrefix, src }) => {
+  const [markup, setMarkup] = useState<string | null>(null);
+  const svgMarkup = useMemo(
+    () => (markup ? prefixSvgIds(markup, idPrefix) : null),
+    [idPrefix, markup]
+  );
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch(src)
+      .then((response) => (response.ok ? response.text() : null))
+      .then((loadedMarkup) => {
+        if (isMounted) {
+          setMarkup(loadedMarkup);
+        }
+      })
+      .catch(() => {
+        if (isMounted) {
+          setMarkup(null);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [src]);
+
+  if (!svgMarkup) {
+    return <img src={src} alt="" />;
+  }
+
+  return (
+    <span
+      className="rules-inline-svg"
+      aria-hidden="true"
+      dangerouslySetInnerHTML={{ __html: svgMarkup }}
+    />
+  );
+};
 
 export const StartScreen: React.FC<StartScreenProps> = ({ width, height, onStart }) => {
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
@@ -60,7 +112,7 @@ export const StartScreen: React.FC<StartScreenProps> = ({ width, height, onStart
                 aria-label="Previous slide"
                 onClick={goToPreviousSlide}
               >
-                <img src={resolvePublicUrl("rules/button_left.svg")} alt="" />
+                <InlineSvgImage idPrefix="rules-left-arrow" src={resolvePublicUrl("rules/button_left.svg")} />
               </button>
 
               <div className="rules-image-slot">
@@ -90,7 +142,7 @@ export const StartScreen: React.FC<StartScreenProps> = ({ width, height, onStart
                 aria-label="Next slide"
                 onClick={goToNextSlide}
               >
-                <img src={resolvePublicUrl("rules/button_right.svg")} alt="" />
+                <InlineSvgImage idPrefix="rules-right-arrow" src={resolvePublicUrl("rules/button_right.svg")} />
               </button>
             </div>
 
